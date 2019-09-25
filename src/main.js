07e71bc6e10c32a6f1dbb9e8491a69d6c3b49a01
Zeal29 <ali029092@gmail.com>
2019-09-25 13:20:10 +0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import router from './router'
+
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min'
+// import 'material-icons/css/material-icons.min.css'
+import 'material-icons/iconfont/material-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+    <nav class="nav-extended indigo" >
+      <div class=" nav-wrapper valign-wrapper" style="height: 100px">
+        <a href="#" class="brand-logo ml-3 title">Zeal Smoothies</a>
+
+          <a class="btn-floating btn-large halfway-fab waves-effect waves-light pink">
+            <i class="material-icons">add</i>
+          </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+
+<style scoped >
+nav > .nav-wrapper{
+padding: 0 20px;
+}
+</style >
