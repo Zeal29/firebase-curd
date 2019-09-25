8596c24b96c52e5ba9f27120bdc377de245895a1
Zeal29 <ali029092@gmail.com>
2019-09-25 13:34:50 +0500

    add dist because i build

---
+++ dist/precache-manifest.7b7d353307a6957d0116dcf385b75d4a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "16edbf3377c979d61191",
+    "url": "/js/about.bc9404ed.js"
+  },
+  {
+    "revision": "feacbef9e1f55a1866cb",
+    "url": "/css/app.3eac3abb.css"
+  },
+  {
+    "revision": "feacbef9e1f55a1866cb",
+    "url": "/js/app.f4b7af61.js"
+  },
+  {
+    "revision": "60c27d3f5029c62d3ff1",
+    "url": "/css/chunk-vendors.6cbe3e9a.css"
+  },
+  {
+    "revision": "60c27d3f5029c62d3ff1",
+    "url": "/js/chunk-vendors.6c0e065c.js"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "7eb4e0909214d8a47e5db773f17d5de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
